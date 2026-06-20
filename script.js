81e4d606351d60ab3855951b3c0fd5ce275425e2
jao05072006 <anderlanedilene@gmail.com>
2026-06-20 14:03:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,86 @@
-function abrirCarta(numero) {
+function abrirCarta(numero){
 
-    if (numero == 1) {
-        alert("Carta da Foto 1 ❤️");
+    const titulo = document.getElementById("tituloCarta");
+    const texto = document.getElementById("textoCarta");
+
+    if(numero == 1){
+
+        titulo.innerHTML = "Nossa primeira foto 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 1.`;
+
     }
 
-    if (numero == 2) {
-        alert("Carta da Foto 2 ❤️");
+    if(numero == 2){
+
+        titulo.innerHTML = "Foto 2 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 2.`;
+
     }
 
-    if (numero == 3) {
-        alert("Carta da Foto 3 ❤️");
+    if(numero == 3){
+
+        titulo.innerHTML = "Foto 3 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 3.`;
+
     }
 
-    if (numero == 4) {
-        alert("Carta da Foto 4 ❤️");
+    if(numero == 4){
+
+        titulo.innerHTML = "Foto 4 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 4.`;
+
     }
 
-    if (numero == 5) {
-        alert("Carta da Foto 5 ❤️");
+    if(numero == 5){
+
+        titulo.innerHTML = "Foto 5 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 5.`;
+
     }
 
-    if (numero == 6) {
-        alert("Carta da Foto 6 ❤️");
+    if(numero == 6){
+
+        titulo.innerHTML = "Foto 6 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 6.`;
+
     }
 
-    if (numero == 7) {
-        alert("Carta da Foto 7 ❤️");
+    if(numero == 7){
+
+        titulo.innerHTML = "Foto 7 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 7.`;
+
     }
 
-    if (numero == 8) {
-        alert("Carta da Foto 8 ❤️");
+    if(numero == 8){
 
-     }
+        titulo.innerHTML = "Foto 8 💜";
 
-    if (numero == 9) {
-        alert("Carta da Foto 9 ❤️");
+        texto.innerHTML = `Aqui vai a carta da foto 8.`;
+
     }
+
+    if(numero == 9){
+
+        titulo.innerHTML = "Foto 9 💜";
+
+        texto.innerHTML = `Aqui vai a carta da foto 9.`;
+
+    }
+
+    document.getElementById("carta").style.display = "flex";
+
+}
+
+function fecharCarta(){
+
+    document.getElementById("carta").style.display = "none";
 
 }
