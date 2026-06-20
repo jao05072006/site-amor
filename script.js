73eab5b132985f8f6f7da660f0d126af4ec0a0ac
jao05072006 <anderlanedilene@gmail.com>
2026-06-20 12:30:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function abrirCarta(numero) {
+
+    if (numero == 1) {
+        alert("Carta da Foto 1 ❤️");
+    }
+
+    if (numero == 2) {
+        alert("Carta da Foto 2 ❤️");
+    }
+
+    if (numero == 3) {
+        alert("Carta da Foto 3 ❤️");
+    }
+
+    if (numero == 4) {
+        alert("Carta da Foto 4 ❤️");
+    }
+
+    if (numero == 5) {
+        alert("Carta da Foto 5 ❤️");
+    }
+
+    if (numero == 6) {
+        alert("Carta da Foto 6 ❤️");
+    }
+
+    if (numero == 7) {
+        alert("Carta da Foto 7 ❤️");
+    }
+
+    if (numero == 8) {
+        alert("Carta da Foto 8 ❤️");
+    }
+
+}
